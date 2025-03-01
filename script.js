f5fa896a9720ac82470ba0c45f3260d6042c6f5b
kodejeet <roshansahx2003@gmail.com>
2025-03-01 17:27:46 +0530

    Frontend Code

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// js 
+//  code to disable the delete icon from getting deleted while typing
+// document.addEventListener("DOMContentLoaded", function () {
+//     const editor = document.getElementById("editor");
+
+//     editor.addEventListener("input", function () {
+//         if (editor.innerHTML.trim() === "") {
+//             editor.innerHTML = '<i class="fa-solid fa-trash delete-icon"></i>';
+//         }
+//     });
+// });
