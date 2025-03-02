1749702a910e66b32de36936a18de1f6e6d47748
kodejeet <roshansahx2003@gmail.com>
2025-03-02 15:01:25 +0530

    added features

--- script.js
+++ script.js
@@ -8,3 +8,22 @@
 //         }
 //     });
 // });
+
+const textArea = document.querySelector(".txtArea");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".editor");
+
+createBtn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let icon = document.createElement("i");
+    inputBox.className = "editor";
+    inputBox.setAttribute("contenteditable", "true");
+    icon.src = "fa-regular fa-pen-to-square";
+    textArea.appendChild(inputBox).appendChild(icon);
+});
+
+textArea.addEventListener("click", function(e){
+    if(e.target.tagName === "icon"){
+        e.target.parentElement.remove();
+    }
+})
